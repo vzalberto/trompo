5c262d71454879c92be38755cb7b6eb41681dddb
Alberto <vzalberto@msn.com>
2019-10-12 15:56:48 -0500

    toggle functions

--- src/index.js
+++ src/index.js
@@ -1,66 +1,15 @@
 import ReactDOM from 'react-dom'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Illustration, Cylinder } from 'react-zdog'
 
-import { StateProvider, useStateValue } from './state';
 import './styles.css'
 
 // TAU representa una vuelta completa a un círculo, a.k.a. 360º
 const TAU = Math.PI * 2
 
-const discos = {
-  a: [{diameter:70,backface:"#f3de72", }, {diameter:60,backface:"#782d21"}, {diameter:10,backface:"#e5af2a", color:"#71471f"}],
-  b: [{diameter:20,backface:"#63b043", color:"#377d1e"}],
-  c: [{diameter:50,backface:"#c1572f"}, {diameter:40,backface:"#ffa500", color:"#573a29"}]
-}
+const Trompo = (props) => {
 
-function Trompo (props) {
-  const [discs, setDiscs] = useState(props.discs)
-  const [aux, setAux] = useState({})
-
-  const initialState = {
-    towers: [
-      [{diameter:70,backface:"#f3de72", }, {diameter:60,backface:"#782d21"}, {diameter:10,backface:"#e5af2a", color:"#71471f"}],
-      [{diameter:20,backface:"#63b043", color:"#377d1e"}],
-      [{diameter:50,backface:"#c1572f"}, {diameter:40,backface:"#ffa500", color:"#573a29"}]
-    ]
-  };
-  
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'changeTheme':
-        return {
-          ...state,
-          theme: action.newTheme
-        };
-        
-      default:
-        return state;
-    }
-  };
- 
-
-  // Elimina el último elemento del arreglo y lo asigna al global state
-  const pizza = () => {
-
-    if(Object.entries(aux).length === 0 && aux.constructor === Object){
-      //Se toma el último disco en un auxiliar
-      const disc = discs[discs.length - 1]
-      setAux(disc)
-
-      //Se actualiza el arreglo de discos
-      setDiscs(discs.slice(0, discs.length - 1))
-    }
-    else {
-      //Si ya se tiene un auxiliar, se añade al arreglo de discos y se elimina del state
-      setDiscs([...discs, aux])
-      setAux({})
-    }
-  };
-
-  useEffect(()=>{})
-
-  const discos = discs.map((d,i) => {
+  const discos = props.discs ? props.discs.map((d,i) => {
     return <Cylinder 
       key={i} 
       diameter={d.diameter} 
@@ -69,22 +18,47 @@ function Trompo (props) {
       color={d.color || "#7c2c22"} 
       stroke={false} 
       backface={d.backface} />
-  });
+  }) : '';
 
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <Illustration onClick={pizza} className={"portal"} zoom={props.zoom} rotate={{ x: TAU / 8 }}>
+      <Illustration className={"portal"} zoom={props.zoom} rotate={{ x: TAU / 8 }}>
         {discos}
       </Illustration>
-    </StateProvider>
- )
+  )
 }
+
+const Hanoi = () => {
+  const initDiscos = {
+    a: [{diameter:70,backface:"#f3de72", }, {diameter:60,backface:"#782d21"}, {diameter:10,backface:"#e5af2a", color:"#71471f"}],
+    b: [{diameter:20,backface:"#63b043", color:"#377d1e"}],
+    c: [{diameter:50,backface:"#c1572f"}, {diameter:40,backface:"#ffa500", color:"#573a29"}],
+    d: []
+  }
+
+  const [torreA, setTorreA] = useState(initDiscos.a)
+  const [torreB, setTorreB] = useState(initDiscos.b)
+  const [torreC, setTorreC] = useState(initDiscos.c)
+
+  const [aux, setAux] = useState({})
+
+  const toggleA = () => {
+    
+    setTorreA([])
+  }
+
+  return (
+      <div>
+        <div onClick={toggleA}>torre A</div>
+        <Trompo discs={torreA} zoom={3} />
+        <Trompo discs={torreB} zoom={3} />
+        <Trompo discs={torreC} zoom={3} />
+      </div>
+      )
+} 
 
 ReactDOM.render(
   <div id="container">
-    <Trompo discs={discos.a} zoom={3}/>
-    <Trompo discs={discos.b} zoom={3}/>
-    <Trompo discs={discos.c} zoom={3}/>
+    <Hanoi />
   </div>
  ,
   document.getElementById('root')
