762d383655f9b7c0983253b2d80dc7f8009d53db
Alberto <vzalberto@msn.com>
2019-10-12 17:02:20 -0500

    Working MVP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Illustration, Cylinder } from 'react-zdog'
 
 import './styles.css'
@@ -39,19 +39,61 @@ const Hanoi = () => {
   const [torreB, setTorreB] = useState(initDiscos.b)
   const [torreC, setTorreC] = useState(initDiscos.c)
 
-  const [aux, setAux] = useState({})
+  const [aux, setAux] = useState(initDiscos.d)
 
   const toggleA = () => {
-    
-    setTorreA([])
+     if (aux.length === 0){
+      setAux ([torreA.pop()])
+      setTorreA(torreA)
+    }else{
+      torreA.push(aux[0])
+      setTorreA(torreA)
+      setAux([])
+    }
   }
+
+  const toggleB = () => {
+     if (aux.length === 0){
+      setAux ([torreB.pop()])
+      setTorreB(torreB)
+    }else{
+      torreB.push(aux[0])
+      setTorreB(torreB)
+      setAux([])
+    }
+  }
+
+  const toggleC = () => {
+     if (aux.length === 0){
+      setAux ([torreC.pop()])
+      setTorreC(torreC)
+    }else{
+      torreC.push(aux[0])
+      setTorreC(torreC)
+      setAux([])
+    }
+  }
+
+  useEffect(()=>{
+    console.log(aux)
+    console.log('torre A: ', torreA)
+    console.log('torre B: ', torreB)
+    console.log('torre C: ', torreC)
+
+    if(torreA.length == 6|| torreB.length == 6 || torreC.length == 6){
+      alert('🌮🌮🌮')
+      setTorreA(initDiscos.a)
+      setTorreB(initDiscos.b)
+      setTorreC(initDiscos.c)
+    }
+  }, [aux, torreA, torreB, torreC])
 
   return (
       <div>
-        <div onClick={toggleA}>torre A</div>
-        <Trompo discs={torreA} zoom={3} />
-        <Trompo discs={torreB} zoom={3} />
-        <Trompo discs={torreC} zoom={3} />
+        <div className={"hand"}><Trompo discs={aux || []} zoom={1}/></div>
+        <div className={"hanoi"} onClick={toggleA}><Trompo discs={torreA} zoom={3} /></div>
+        <div className={"hanoi"} onClick={toggleB}><Trompo discs={torreB} zoom={3} /></div>
+        <div className={"hanoi"} onClick={toggleC}><Trompo discs={torreC} zoom={3} /></div>
       </div>
       )
 } 
